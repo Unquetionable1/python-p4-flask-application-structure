fe59207d0f9fa37ea468b982055555ecc5cb31f2
Edmound Njoori <eddnjoori@gmail.com>
2024-06-25 11:56:33 +0300

    Complete making a app

--- server/app.py
+++ server/app.py
@@ -1,5 +1,15 @@
-#!/usr/bin/env python3
-
-from flask import Flask
-
+from flask import Flask, render_template
 app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return '<h1>Welcome to my page!</h1>'
+
+@app.route('/<username>')
+def user(username):
+     return f'<h1>Profile for {username}</h1>'
+ 
+if __name__ == '__main__':
+    app.run(host='127.0.0.1', port=8000, debug=True)
+ 
